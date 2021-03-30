2d3b7aff0380d388cb623262b848b07817778bc3
n0y0j <n0y0j77@gmail.com>
2021-03-30 22:37:51 +0900

    feat: Implement price target data function.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer")
 const cheerio = require("cheerio")
+var fs = require('fs')
 
 const getHistoricalData = async (ticker) => {
     
@@ -43,25 +44,61 @@ const getAnalysisData = async (ticker) => {
     const page = await browser.newPage();
     await page.goto(`https://finance.yahoo.com/quote/${ticker}/analysis?p=${ticker}`)
     const content = await page.content();
-    // #Col1-0-AnalystLeafPage-Proxy > section > table:nth-child(2) > tbody > tr:nth-child(5) > td:nth-child(2) > span
+
     const $ = cheerio.load(content);
     const lists = $(`#Col1-0-AnalystLeafPage-Proxy > section`);
     const earningTable = $(lists).find(`table:nth-child(2) > tbody > tr:nth-child(5)`);
     const revenueTable = $(lists).find(`table:nth-child(3) > tbody > tr:nth-child(6)`);
 
-    earningTable.map((index, item) => {
-        console.log($(item).find("td:nth-child(2)").text())
-        console.log($(item).find("td:nth-child(3)").text())
-        console.log($(item).find("td:nth-child(4)").text())
-        console.log($(item).find("td:nth-child(5)").text())
+    const temp = $('script:not([src])');
+
+    console.log(temp[38].children[0].data)
+    
+    // var file = 'text.txt';
+    // fs.open(file,'w',function(err,fd){ if (err) throw err; console.log('file open complete'); });
+
+    fs.writeFile('text.txt', temp[38].children[0].data, 'utf8', function(error) {
+        console.log("write end")
     })
 
-    revenueTable.map((index, item) => {
-        console.log($(item).find("td:nth-child(2)").text())
-        console.log($(item).find("td:nth-child(3)").text())
-        console.log($(item).find("td:nth-child(4)").text())
-        console.log($(item).find("td:nth-child(5)").text())
-    })
-} 
+    // console.log(recoRating)
+    // earningTable.map((index, item) => {
+    //     console.log($(item).find("td:nth-child(2)").text())
+    //     console.log($(item).find("td:nth-child(3)").text())
+    //     console.log($(item).find("td:nth-child(4)").text())
+    //     console.log($(item).find("td:nth-child(5)").text())
+    // })
 
-getAnalysisData('AAPL')
+    // revenueTable.map((index, item) => {
+    //     console.log($(item).find("td:nth-child(2)").text())
+    //     console.log($(item).find("td:nth-child(3)").text())
+    //     console.log($(item).find("td:nth-child(4)").text())
+    //     console.log($(item).find("td:nth-child(5)").text())
+    // })
+}
+
+const getPriceTarget = async (ticker) => {
+    const browser = await puppeteer.launch({
+        headless: true
+    })
+
+    const page = await browser.newPage();
+    await page.goto(`https://www.tipranks.com/stocks/${ticker}/forecast`)
+    const content = await page.content();
+
+    await page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36");
+    
+    const $ = cheerio.load(content);
+    const lists = $(`div.client-components-stock-research-StockPageBox-style__box.stock-box-mobile-border-top-override.client-components-stock-research-analysts-style__priceTarget.client-components-stock-research-style__tabbedStockBox > div > p`);
+
+    const priceTarget = {
+        numberOfAnalysts: parseInt($(lists).find('strong:nth-child(1)').text().split(' ')[0]),
+        average: parseFloat($(lists).find('strong:nth-child(3)').text().replace('$', "").replace(",", "")),
+        high: parseFloat($(lists).find('strong:nth-child(4)').text().replace('$', "").replace(",", "")),
+        low: parseFloat($(lists).find('strong:nth-child(5)').text().replace('$', "").replace(",", "")),
+    }
+
+    console.log(priceTarget)
+}
+
+getPriceTarget('GOOG')
